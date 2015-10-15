a93e1677e404195fb3eaa6615c49a2103d5b8643
zuqini <lizuqiliang@gmail.com>
2015-10-14 23:30:15 -0400

    Upgraded coding animation, added fadeInUp description

--- javascript/index.js
+++ javascript/index.js
@@ -1,43 +1,71 @@
+String.prototype.shuffle = function () {
+    var a = this.split(""),
+        n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
+
 $( document ).ready(function() {
+	$('#mini-title').hide();
 	new Vivus('welcome-svg', {
 			file: 'resources/zuqi.svg',
 			type: 'oneByOne'
 		}, function(vivus) {
+			$('#mini-title').show();
+			$('#mini-title').addClass('animated fadeInUp');
 			typeCode();
 		}
 	);
 
 	//Type some code
-	var code = "<p>(welcome() {</p><p style='text-indent:50px;'>for (int i = 21; i > 0; i--) {</p><p style='text-indent:100px;'>print(\"Welcome!  \");</p><p style='text-indent:50px;'>}</p><p>}());</p>";
+	var code = "<p>(function run() {</p><p style='text-indent:50px;'>while (!message.equals(mystery)) {</p><p style='text-indent:100px;'>message.scramble();</p><p style='text-indent:100px;'>print(message);</p><p style='text-indent:50px;'>}</p><p>}());</p>";
     var i = 0;
-    var j = 0;
     var isTag;
     var codeToType;
-    var codeResult = "";
 
 	function typeCode() {
 	    codeToType = code.slice(0, ++i);
 	    if (codeToType === code) {
-	    	typeResult();
+	    	document.getElementById('type-code').innerHTML = codeToType + '_';
+	    	setTimeout(handleCode, 500);
 	    	return;
 	    }
 	    
-	    document.getElementById('type-code').innerHTML = codeToType;
+	    document.getElementById('type-code').innerHTML = codeToType + '_';
 
 	    var char = codeToType.slice(-1);
 	    if( char === '<' ) isTag = true;
 	    if( char === '>' ) isTag = false;
 
 	    if (isTag) return typeCode();
-	    setTimeout(typeCode, 20);
+	    setTimeout(typeCode, 40);
 	}
 
-	function typeResult() {
-		if(j++ == 21) {
-			return;
-		}
-		codeResult += "Welcome! ";
-		document.getElementById('type-result').innerHTML = codeResult;
-		setTimeout(typeResult, 10);
+	//shuffling
+	var target = "Welcome to my website!";
+	var str = target.shuffle();
+
+	var count = 0;
+	var maxCount = 300;
+
+	function handleCode() {
+		document.getElementById('type-code').innerHTML = codeToType;
+		scrambleWord();
 	}
+
+	function scrambleWord() {
+	 	str = str.shuffle();
+		document.getElementById('code-result').innerHTML = str;
+		if(str == target || count++ === maxCount) {
+	        document.getElementById('code-result').innerHTML = target;
+	        return;
+	    }
+		setTimeout(scrambleWord, 20);
+	};
 });
