89fd79f46e47f7ca99fd689f2b0f80e783a5d995
zuqini <lizuqiliang@gmail.com>
2015-10-18 04:58:37 -0400

    smoother background fadein animation

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@ $( document ).ready(function() {
 			$('#console-window').addClass('animated zoomIn');
 			$('.image-links').addClass('animated fadeInUp');
 			$('.trans-grow').addClass('grow');
-			$('.background-overlay').addClass('background-show');
+			$('.background-view').addClass('background-show');
 			$(window).scrollTop($(window).scrollTop());
 			typeCode();
 		}
