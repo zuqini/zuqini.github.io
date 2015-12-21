e23c17241d565f4a8d7c6996269b5692d7f86ad2
zuqini <zu.q.li@uwaterloo.ca>
2015-12-21 00:12:26 -0500

    added portfolio animation

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
 	$('.trans-grow').addClass('grow');
 	$('.title').addClass('animated fadeInDown');
+	$('#project1').addClass('animated fadeInLeft');
+	$('#project2').addClass('animated fadeInRight');
+	$('#project3').addClass('animated fadeInLeft');
+
+	setTimeout(function(){ $('#project1').show(); }, 200);
+	setTimeout(function(){ $('#project2').show(); }, 500);
+	setTimeout(function(){ $('#project3').show(); }, 800);
 });
