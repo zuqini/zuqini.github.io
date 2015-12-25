5ef261f69ca214b9b7ec580f30591564974c552c
zuqini <lizuqiliang@gmail.com>
2015-12-25 14:27:04 +0800

    improved wording

--- js/index.js
+++ js/index.js
@@ -5,14 +5,7 @@ $( document ).ready(function() {
 	$('#IAmA1').addClass('animated fadeInUp');
 	$('#IAmA2').addClass('animated fadeInUp');
 	$('#IAmA3').addClass('animated fadeInUp');
-	$('.IAmP').addClass('animated fadeInUp');
-
-	$('.greeting').hide();
-	$('.IAmA').hide();
-	$('#IAmA1').hide();
-	$('#IAmA2').hide();
-	$('#IAmA3').hide();
-	$('.IAmP').hide();
+	$('.IAmP').addClass('animated fadeIn');
 
 	new Vivus('welcome-svg', {
 			file: 'resources/zuqi.svg',
@@ -25,11 +18,11 @@ $( document ).ready(function() {
 			$('.fadeInUp-container').show();
 			$(window).scrollTop($(window).scrollTop());
 
-			setTimeout(function(){ $('.IAmA').show(); }, 300);
-			setTimeout(function(){ $('#IAmA1').show(); }, 600);
-			setTimeout(function(){ $('#IAmA2').show(); }, 900);
-			setTimeout(function(){ $('#IAmA3').show(); }, 1200);
-			setTimeout(function(){ $('.IAmP').show(); }, 1500);
+			setTimeout(function(){ $('.IAmA').css("display", "inline-block"); }, 500);
+			setTimeout(function(){ $('#IAmA1').css("display", "inline-block"); }, 800);
+			setTimeout(function(){ $('#IAmA2').css("display", "inline-block"); }, 1100);
+			setTimeout(function(){ $('#IAmA3').css("display", "inline-block"); }, 1400);
+			setTimeout(function(){ $('.IAmP').show(); }, 2000);
 		}
 	);
 
