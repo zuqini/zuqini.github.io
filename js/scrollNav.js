6ba715cd81aac2ab12a3d307c15713f682e47874
zuqini <lizuqiliang@gmail.com>
2016-01-21 23:31:47 -0500

    fix bug where nav bar does not retract

--- js/scrollNav.js
+++ js/scrollNav.js
@@ -1,6 +1,6 @@
 $(function(){
     var lastScrollTop = 0, delta = 5;
-    $(window).scroll(function(event){
+    $('.foreground-view').scroll(function(event){
        var st = $(this).scrollTop();
        
        if(Math.abs(lastScrollTop - st) <= delta)
