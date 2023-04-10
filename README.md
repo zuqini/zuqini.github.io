293206642104e2b512f86b958eeafefe00e8bc36
zuqini <lizuqiliang@gmail.com>
2023-04-10 01:53:22 -0700

    nuke and add new website

--- README.md
+++
@@ -1,2 +0,0 @@
-# zuqini.github.io
-My personal website.
--- contact/js/contact.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-	$('.trans-grow').addClass('grow');
-	$('.title').addClass('animated fadeInDown');
-	$('.contact-info').addClass('animated fadeInUp');
-	$('.contact-message').addClass('animated fadeInUp');
-});
