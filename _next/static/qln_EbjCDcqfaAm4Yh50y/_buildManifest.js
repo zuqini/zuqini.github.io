cab8f22327d5953fbcef26ae7a567e6e039c4832
zuqini <lizuqiliang@gmail.com>
2023-04-12 11:55:11 -0700

    update wording

---
+++ _next/static/qln_EbjCDcqfaAm4Yh50y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-276396e85e3a8c2a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
