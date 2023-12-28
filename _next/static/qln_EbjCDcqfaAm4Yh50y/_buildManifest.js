7182ad9c5e729c53a87674d827a89a2ffc0d77e2
zuqini <lizuqiliang@gmail.com>
2023-12-27 22:48:03 -0800

    update website for 2024

--- _next/static/qln_EbjCDcqfaAm4Yh50y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-276396e85e3a8c2a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
