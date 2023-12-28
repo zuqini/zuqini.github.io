e74c867144a2288731484a1c574be33135eaf755
zuqini <lizuqiliang@gmail.com>
2023-12-28 11:49:33 -0800

    ml-4 list

--- _next/static/gtz8oiJhdFvwmuBlhVieD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d0e1dcf327912c70.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
